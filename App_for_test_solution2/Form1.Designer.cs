550efc00f16bad58cb68dc3cd428b8ba35e5d059
KajimaSoys <OneDudeAdam@mail.ru>
2019-06-29 22:41:11 +0300

    V.0.1
    Создана нерабочая форма запуска программы

---
+++ App_for_test_solution2/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace App_for_test_solution2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TestName = new System.Windows.Forms.Label();
+            this.beginButton = new System.Windows.Forms.Button();
+            this.TestOptions = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // TestName
+            // 
+            this.TestName.Font = new System.Drawing.Font("Calibri", 24F);
+            this.TestName.Location = new System.Drawing.Point(128, 51);
+            this.TestName.Name = "TestName";
+            this.TestName.Size = new System.Drawing.Size(568, 81);
+            this.TestName.TabIndex = 1;
+            this.TestName.Text = "Тест по инструментальным средствам информационных систем";
+            this.TestName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.TestName.UseCompatibleTextRendering = true;
+            this.TestName.UseMnemonic = false;
+            this.TestName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // beginButton
+            // 
+            this.beginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.beginButton.Location = new System.Drawing.Point(269, 231);
+            this.beginButton.Name = "beginButton";
+            this.beginButton.Size = new System.Drawing.Size(289, 79);
+            this.beginButton.TabIndex = 2;
+            this.beginButton.Text = "Запуск теста";
+            this.beginButton.UseVisualStyleBackColor = true;
+            this.beginButton.Click += new System.EventHandler(this.beginButton_Click);
+            // 
+            // TestOptions
+            // 
+            this.TestOptions.AutoSize = true;
+            this.TestOptions.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TestOptions.Location = new System.Drawing.Point(276, 154);
+            this.TestOptions.Name = "TestOptions";
+            this.TestOptions.Size = new System.Drawing.Size(272, 33);
+            this.TestOptions.TabIndex = 3;
+            this.TestOptions.Text = "15 вопросов; 30 минут";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TestOptions);
+            this.Controls.Add(this.beginButton);
+            this.Controls.Add(this.TestName);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Тест по технологиям программирования";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.Label TestName;
+        private System.Windows.Forms.Button beginButton;
+        private System.Windows.Forms.Label TestOptions;
+    }
+}
+
