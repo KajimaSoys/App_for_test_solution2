1e8c897c42e84c9c6e932a826c297366fefaa429
KajimaSoys <OneDudeAdam@mail.ru>
2019-07-03 01:32:35 +0300

    V.0.3 (Stable)
    Добавлено:
    - Рандомный выбор вопросов и ответов из базы данных
    - Кнопка button_5 (Следующий вопрос), которая отвечает за повторный вызов элементов для кнопок выбора ответа и текста вопроса
    - Создана конечная форму, в которой будет показано количество верных ответов (надо допилить)
    - База данных полностью составлена и перенесена в Microsoft Access
    
    Возможности, которые реализую в дальнейших версиях:
    - Перекину код инициализации БД в файл DataBase.cs
    - Перекину код работы с элементами БД в файл Storage.cs
    - Добавлю кнопку закрытия файла в конечную форму, а также в кнопку "выходToolStripMenuItem"
    - Добавлю ограничение по времени, по истечению которого необходимо, чтобы исходная форма изменилась так, что в ней будет написано, что время истекло и выводилось количество верных ответов с оценкой
    - Добавлю оставшееся время
    - Добавить возможность задавать в начале программы количество вопросов и в соответствии с этим показывать доступное время на выполнение теста
    - Добавлю возможность возврата к предыдущему вопросу, причем он должен начинать работать со второго вопроса (Задам проверкой: Если id = 0, то форма, где можно только перейти вперед, если >0, то можно вперед и назад). Подсчет верных ответов можно реализовать так: Если ответ совпадает с right_answer то счетчик инкрементируется, в обратном случае декрементция. В этом случае необходимо будет добавить кнопку "Завершить тест". Добавлю статистику вопросов отвечено/всего. Сделаю так, чтобы на определенном вопросе запоминалось, какая кнопка должна подсвечиваться и была выбрана. Значение заданного пользователем ответа должно запоминаться в какой-то переменной, соответственно исходя из нее и буду подсвечивать нужную кнопку
    - Добавить возможность повторного выполнения теста
    
    Программа готова к работе. Осталось закончить немного графических задач, а затем перейти к дополнительным.

--- App_for_test_solution2/Form1.Designer.cs
+++ App_for_test_solution2/Form1.Designer.cs
@@ -41,6 +41,7 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.endButton = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -90,7 +91,7 @@
             // beginButton
             // 
             this.beginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.beginButton.Location = new System.Drawing.Point(263, 231);
+            this.beginButton.Location = new System.Drawing.Point(256, 229);
             this.beginButton.Name = "beginButton";
             this.beginButton.Size = new System.Drawing.Size(289, 79);
             this.beginButton.TabIndex = 2;
@@ -100,21 +101,21 @@
             // 
             // TestOptions
             // 
-            this.TestOptions.AutoSize = true;
             this.TestOptions.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.TestOptions.Location = new System.Drawing.Point(272, 154);
+            this.TestOptions.Location = new System.Drawing.Point(58, 140);
             this.TestOptions.Name = "TestOptions";
-            this.TestOptions.Size = new System.Drawing.Size(272, 33);
+            this.TestOptions.Size = new System.Drawing.Size(693, 89);
             this.TestOptions.TabIndex = 3;
             this.TestOptions.Text = "15 вопросов; 30 минут";
+            this.TestOptions.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.Control;
             this.button1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(58, 140);
+            this.button1.Location = new System.Drawing.Point(22, 140);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(328, 89);
+            this.button1.Size = new System.Drawing.Size(370, 89);
             this.button1.TabIndex = 4;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = false;
@@ -125,9 +126,9 @@
             // 
             this.button2.BackColor = System.Drawing.SystemColors.Control;
             this.button2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(423, 140);
+            this.button2.Location = new System.Drawing.Point(406, 140);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(328, 89);
+            this.button2.Size = new System.Drawing.Size(370, 89);
             this.button2.TabIndex = 5;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = false;
@@ -138,9 +139,9 @@
             // 
             this.button3.BackColor = System.Drawing.SystemColors.Control;
             this.button3.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button3.Location = new System.Drawing.Point(58, 249);
+            this.button3.Location = new System.Drawing.Point(22, 249);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(328, 89);
+            this.button3.Size = new System.Drawing.Size(370, 89);
             this.button3.TabIndex = 6;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = false;
@@ -151,9 +152,9 @@
             // 
             this.button4.BackColor = System.Drawing.SystemColors.Control;
             this.button4.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button4.Location = new System.Drawing.Point(423, 249);
+            this.button4.Location = new System.Drawing.Point(406, 249);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(328, 89);
+            this.button4.Size = new System.Drawing.Size(370, 89);
             this.button4.TabIndex = 7;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = false;
@@ -164,7 +165,7 @@
             // 
             this.button5.BackColor = System.Drawing.SystemColors.Control;
             this.button5.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button5.Location = new System.Drawing.Point(263, 359);
+            this.button5.Location = new System.Drawing.Point(256, 359);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(289, 59);
             this.button5.TabIndex = 8;
@@ -173,24 +174,37 @@
             this.button5.Visible = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // endButton
+            // 
+            this.endButton.BackColor = System.Drawing.SystemColors.Control;
+            this.endButton.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.endButton.Location = new System.Drawing.Point(255, 359);
+            this.endButton.Name = "endButton";
+            this.endButton.Size = new System.Drawing.Size(289, 59);
+            this.endButton.TabIndex = 9;
+            this.endButton.Text = "button6";
+            this.endButton.UseVisualStyleBackColor = false;
+            this.endButton.Visible = false;
+            this.endButton.Click += new System.EventHandler(this.endButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.endButton);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.TestOptions);
             this.Controls.Add(this.beginButton);
             this.Controls.Add(this.TestName);
             this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.TestOptions);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            //this.Text = "Тест по технологиям программирования";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -213,6 +227,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button endButton;
     }
 }
 
--- App_for_test_solution2/Form1.cs
+++ App_for_test_solution2/Form1.cs
@@ -25,11 +25,12 @@ namespace App_for_test_solution2
         Color default_color;
         Color active_color;
 
-        int active_question = -1;
+        int active_question = 0;
         int active_answer = 0;
         int right_answer = 0;
         int count_of_right_answer = 0;
 
+        string[,] storage = new string[15, 6];
 
         public Form1()
         {
@@ -49,44 +50,98 @@ namespace App_for_test_solution2
 
         private void beginButton_Click(object sender, EventArgs e)
         {
-            //Вызываем данные из базы
+            Array();
+
+            //Вызываем данные из массива элементов, которые получили от базы данных
             DataRequest();
+
+            active_question++;
 
             //Задаем видимость для элементов формы после нажатия кнопки Начать тест
             beginButton.Visible = false;
             TestOptions.Visible = false;
-            TestName.Font = new Font("Calibri", 20F);
+            TestName.Font = new Font("Calibri", 18F);
             button5.Text = "Следующий вопрос";
             button1.Visible = true;
             button2.Visible = true;
             button3.Visible = true;
             button4.Visible = true;
             button5.Visible = true;
+            
+        }
+
+        //создадим массив для хранения вопросов
+        public void Array()
+        {
+            Random rand = new Random();
+
+            //Создали массив для хранения случайных номеров вопрсов
+            int[] id = new int[15];
+            int buf;
+            bool contains;
+            for (int i = 0; i < id.Length; i++)
+            {
+                do
+                {
+                    buf = rand.Next(1, 51);
+                    contains = false;
+                    for (int index = 0; index < i; index++)
+                    {
+                        int n = id[index];
+                        if (n == buf)
+                        {
+                            contains = true;
+                            break;
+                        }
+                    }
+
+                } while (contains);
+                id[i] = buf;
+            }
+
+            //создадим массив для хранения самих данных вопроса
+            
+            for (int i = 0; i < 15; i++)
+            {
+                //Текст запроса в БД
+                string testAnswerRequest = "SELECT text_of_question, text_of_1_answer, text_of_2_answer, text_of_3_answer, text_of_4_answer, right_answer FROM TestStorage WHERE number = "+id[i];
+               
+                // создаем объект OleDbCommand для выполнения запроса к БД MS Acces
+                OleDbCommand command = new OleDbCommand(testAnswerRequest, myConnection);
+
+                //Выполняем запрос
+                OleDbDataReader reader = command.ExecuteReader();
+
+                //Сохраняем результат запроса в многомерный массив storage
+                while (reader.Read())
+                {
+                    storage[i,0] = (reader[0].ToString());
+                    storage[i, 1] = (reader[1].ToString());
+                    storage[i, 2] = (reader[2].ToString());
+                    storage[i, 3] = (reader[3].ToString());
+                    storage[i, 4] = (reader[4].ToString());
+                    storage[i, 5] = reader[5].ToString();
+                }
+                reader.Close();
+            }
         }
 
         //запрос данных из БД
         void DataRequest()
         {
-            //Текст запроса в БД
-            string testAnswerRequest = "SELECT text_of_question, text_of_1_answer, text_of_2_answer, text_of_3_answer, text_of_4_answer, right_answer FROM TestStorage ORDER BY number = 1";
+            //for (int i = 0; i<)
 
-            // создаем объект OleDbCommand для выполнения запроса к БД MS Acces
-            OleDbCommand command = new OleDbCommand(testAnswerRequest, myConnection);
-
-            //Выполняем запрос
-            OleDbDataReader reader = command.ExecuteReader();
-            
             //Выводим результат в TestName и в кнопки выбора ответа
-            while (reader.Read())
-            {
-                TestName.Text = (reader[0].ToString());
-                button1.Text = (reader[1].ToString());
-                button2.Text = (reader[2].ToString());
-                button3.Text = (reader[3].ToString());
-                button4.Text = (reader[4].ToString());
-                right_answer = Convert.ToInt32(reader[5].ToString());
-            }
-            reader.Close();
+            //while (reader.Read())
+            //{
+            TestName.Text = storage[active_question, 0];
+            button1.Text = storage[active_question, 1];
+            button2.Text = storage[active_question, 2];
+            button3.Text = storage[active_question, 3];
+            button4.Text = storage[active_question, 4];
+            right_answer = Convert.ToInt32(storage[active_question, 5]);
+            //}
+            //reader.Close();
         }
         
         //Окрашиваем кнопки в стандартный цвет
@@ -134,8 +189,36 @@ namespace App_for_test_solution2
                 count_of_right_answer++;
             }
             SetButtonDefaultColor();
+            DataRequest();
+            if (active_question < 14)
+            {
+                DataRequest();
+                active_question++;
+            }
+            else
+            {
+                button5.Visible = false;
+                endButton.Visible = true;
+                endButton.Text = "Завершить тест";
+            }
         }
 
+        private void endButton_Click(object sender, EventArgs e)
+        {
+            if (active_answer == right_answer)
+            {
+                count_of_right_answer++;
+            }
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+            TestOptions.Visible = true;
+            TestName.Text = "Тест окончен!!!";
+            TestName.Font = new Font("Calibri", 26F);
+            TestOptions.Text = "Количество верных ответов: " + count_of_right_answer + " / 15";
+            endButton.Visible = false;
+        }
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             //Закрываем соединение с базой данных
